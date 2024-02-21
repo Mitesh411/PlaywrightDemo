589b626a7d080d48633bb2aa59593f236725786b
Mitesh411 <miteshdandade@ymail.com>
2024-02-21 18:14:38 +0530

    first commit

---
+++ tests/get.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test("GET booking summary", async ({ request }) => {
+  const response = await request.get(
+    "https://automationintesting.online/booking/summary?roomid=1"
+  );
+
+  expect(response.status()).toBe(200);
+  const body = await response.json();
+  console.log(JSON.stringify(body));
+});
